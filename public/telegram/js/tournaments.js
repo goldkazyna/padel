6a373a5f8503bb35130217073d756f6a7e1eafca
Denis Dudnikov <goldkazyna@gmail.com>
2026-01-26 17:22:13 +0500

    Акция закончилась

--- public/telegram/js/tournaments.js
+++ public/telegram/js/tournaments.js
@@ -194,6 +194,7 @@ async function refreshTournament() {
  * Рендер детальной страницы турнира
  */
 function renderTournamentDetail(data) {
+	alert('Участники: ' + JSON.stringify(data.participants));
     const { tournament, participants, is_registered, registration_status, can_register } = data;
     const container = document.getElementById('tournament-detail-content');
     if (!container) return;
