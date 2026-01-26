f7dc8269c93e3f196cc95c2b3a3ae2daa40a2eb0
Denis Dudnikov <goldkazyna@gmail.com>
2026-01-26 16:57:21 +0500

    fix: правильная сетка 1/4 финала - A1 и B1 встречаются только в финале1

--- app/Http/Controllers/Api/TelegramWebhookController.php
+++ app/Http/Controllers/Api/TelegramWebhookController.php
@@ -6,22 +6,34 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
 
 class TelegramWebhookController extends Controller
 {
-    public function handle(Request $request)
-    {
-        $update = $request->all();
-        
-        Log::info('Telegram webhook', $update);
+	public function handle(Request $request)
+	{
+		$update = $request->all();
+		
+		Log::info('Telegram webhook', $update);
 
-        // Проверяем что это сообщение с контактом
-        if (isset($update['message']['contact'])) {
-            $this->handleContact($update['message']);
-        }
+		// Контакт (номер телефона)
+		if (isset($update['message']['contact'])) {
+			$this->handleContact($update['message']);
+			return response()->json(['ok' => true]);
+		}
 
-        return response()->json(['ok' => true]);
-    }
+		// Текстовые сообщения (кнопки меню)
+		if (isset($update['message']['text'])) {
+			$text = $update['message']['text'];
+			$chatId = $update['message']['chat']['id'];
+
+			if (in_array($text, ['🏆 Турниры', '👤 Мой профиль', '🎾 Открыть приложение', '/start', '🏠 Главное меню', 'ℹ️ Помощь'])) {
+				$this->sendWebAppButton($chatId);
+			}
+		}
+
+		return response()->json(['ok' => true]);
+	}
 
     protected function handleContact(array $message)
     {
@@ -45,4 +57,19 @@ class TelegramWebhookController extends Controller
             Log::info('Phone saved', ['user_id' => $user->id, 'phone' => $phone]);
         }
     }
+	
+	protected function sendWebAppButton(int $chatId)
+	{
+		$botToken = config('services.telegram.bot_token');
+		
+		Http::post("https://api.telegram.org/bot{$botToken}/sendMessage", [
+			'chat_id' => $chatId,
+			'text' => '🎾 Откройте приложение:',
+			'reply_markup' => json_encode([
+				'inline_keyboard' => [[
+					['text' => '🎾 Открыть приложение', 'web_app' => ['url' => 'https://padel-p.kz/telegram/']]
+				]]
+			])
+		]);
+	}
 }
