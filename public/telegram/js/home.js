8d9c43eb272f2d849e4ce1a294b8457d749ce649
Denis Dudnikov <goldkazyna@gmail.com>
2026-01-27 10:32:56 +0500

    Главная турниры

--- public/telegram/js/home.js
+++ public/telegram/js/home.js
@@ -108,14 +108,15 @@ async function renderNextTournament() {
                                 ${time}
                             </span>
                             <span class="meta-item ${isFull ? 'full' : ''}">
-                                <svg class="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                                    <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"/>
-                                    <circle cx="9" cy="7" r="4"/>
-                                    <path d="M23 21v-2a4 4 0 0 0-3-3.87"/>
-                                    <path d="M16 3.13a4 4 0 0 1 0 7.75"/>
-                                </svg>
-                                ${tournament.participants_count}/${tournament.max_participants}
-                            </span>
+								<svg class="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+									<path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"/>
+									<circle cx="9" cy="7" r="4"/>
+									<path d="M23 21v-2a4 4 0 0 0-3-3.87"/>
+									<path d="M16 3.13a4 4 0 0 1 0 7.75"/>
+								</svg>
+								${tournament.participants_count}/${tournament.max_participants}
+								${isFull ? '<span class="full-badge">Мест нет</span>' : ''}
+							</span>
                             <span class="meta-item">
                                 <svg class="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                                     <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/>
